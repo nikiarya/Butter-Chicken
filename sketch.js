bdde837ff260e2c667a52576592c4ac125538a12
nikiarya <Nikitaarya1967@gmail.com>
2020-08-07 13:12:45 -0400

    creating recpies

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var bird, slingShot;
+var bird, sling1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -37,9 +37,9 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    sling1 = new SlingShot(bird.body,{x:150,y:105});
+    
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -63,6 +63,14 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    sling1.display();
+
+}
+
+function mouseDragged() {
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased() {
+    sling1.fly();
 }
